cb71c931d890f5479f9a2301189f1c7c8f939786
RamonKea-inh <687187@student.inholland.nl>
2024-12-25 01:42:46 +0100

    Delete types.go
    made a model for this in models.go

--- internal/database/repositories/types.go
+++
@@ -1,9 +0,0 @@
-package repositories
-
-// MessageQueryOptions defines the parameters for querying messages
-type MessageQueryOptions struct {
-	Limit     int    // Maximum number of messages to return
-	Cursor    string // Token for pagination
-	Direction string // Direction of pagination (next/previous)
-	Search    string // Search term for filtering messages
-}
